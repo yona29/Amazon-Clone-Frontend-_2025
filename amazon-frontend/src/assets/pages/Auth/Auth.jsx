0ccb8466650d0d2424a2804ef19538e2138a2890
yona29 <yoniyehabtishleg@gmail.com>
2025-10-16 18:44:38 -0400

    update auth part

--- amazon-frontend/src/assets/pages/Auth/Auth.jsx
+++ amazon-frontend/src/assets/pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import classes from "./Signup.module.css";
+import classes from "./SignUp.module.css";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { auth } from "../../Utility/firebase";
 import {
